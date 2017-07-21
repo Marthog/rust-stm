6b3acaf4b9b8971fa0dffca7eeb32e8752b07f83
Marthog <Marthog@users.noreply.github.com>
2017-07-21 14:08:42 +0200

    Added functions `unwrap_or_retry`, `guard`, `optionally`.

--- src/lib.rs
+++ src/lib.rs
@@ -139,6 +139,70 @@ where F: Fn(&mut Transaction) -> StmResult<T>
     Transaction::with(f)
 }
 
+#[inline]
+/// Unwrap `Option` or call retry if it is `None`.
+///
+/// # Example
+///
+/// ```
+/// use stm::*;
+///
+/// let x = atomically(|_tx|
+///     unwrap_or_retry(Some(42))
+/// );
+/// assert_eq!(x, 42);
+/// ```
+pub fn unwrap_or_retry<T>(option: Option<T>) 
+    -> StmResult<T> {
+    match option {
+        Some(x) => Ok(x),
+        None    => retry()
+    }
+}
+
+#[inline]
+/// Retry until a the condition holds.
+///
+/// # Example
+///
+/// ```
+/// use stm::*;
+/// let x = atomically(|_tx| {
+///     guard(true)?; // guard(true) always succeeds.
+///     Ok(42)
+/// });
+/// assert_eq!(x, 42);
+/// ```
+pub fn guard(cond: bool) -> StmResult<()> {
+    if cond {
+        Ok(())
+    } else {
+        retry()
+    }
+}
+
+#[inline]
+/// Optionally run a STM action. If `f` fails with a `retry()`, it does 
+/// not cancel the whole transaction, but returns `None`.
+///
+/// # Example
+///
+/// ```
+/// use stm::*;
+/// let x:Option<i32> = atomically(|tx| 
+///     optionally(tx, |_| retry()));
+/// assert_eq!(x, None);
+/// ```
+pub fn optionally<T,F>(tx: &mut Transaction, f: F) -> StmResult<Option<T>>
+    where F: Fn(&mut Transaction) -> StmResult<T>
+{
+    tx.or( 
+        |t| f(t).map(Some),
+        |_| Ok(None)
+    )
+}
+
+
 #[test]
 fn test_infinite_retry() {
     let terminated = test::terminates(300, || { 
@@ -196,6 +260,7 @@ fn test_threaded() {
 
     assert_eq!(42, x);
 }
+
 
 /// test if a STM calculation is rerun when a Var changes while executing
 #[test]
@@ -305,3 +370,41 @@ fn test_or_nested_second() {
     assert_eq!(x, 42);
 }
 
+#[test]
+fn unwrap_some() {
+    let x = Some(42);
+    let y = atomically(|_| unwrap_or_retry(x));
+    assert_eq!(y, 42);
+}
+
+#[test]
+fn unwrap_none() {
+    let x: Option<i32> = None;
+    assert_eq!(unwrap_or_retry(x), retry());
+}
+
+#[test]
+fn guard_true() {
+    let x = guard(true);
+    assert_eq!(x, Ok(()));
+}
+
+#[test]
+fn guard_false() {
+    let x = guard(false);
+    assert_eq!(x, retry());
+}
+
+#[test]
+fn optionally_succeed() {
+    let x = atomically(|t| 
+        optionally(t, |_| Ok(42)));
+    assert_eq!(x, Some(42));
+}
+
+#[test]
+fn optionally_fail() {
+    let x:Option<i32> = atomically(|t| 
+        optionally(t, |_| retry()));
+    assert_eq!(x, None);
+}
