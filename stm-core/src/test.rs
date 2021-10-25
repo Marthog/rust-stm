bb5c54e332d1fc162f2826dc345e098a4dda2566
Akosh Farkash <aakoshh@gmail.com>
2021-10-26 00:17:15 +0100

    Rename to run_async to avoid name clash with async.

--- stm-core/src/test.rs
+++ stm-core/src/test.rs
@@ -1,5 +1,5 @@
 //! This module contains helpers for various tests.
-//! Quite a lot of tests run operations asynchonously and need to check 
+//! Quite a lot of tests run operations asynchonously and need to check
 //! for deadlocks. We do this by waiting a certain amount of time for completion.
 //!
 //! This module contains some helpers that simplify other tests.
@@ -12,22 +12,22 @@ use std::sync::mpsc::channel;
 ///
 /// If the function does not terminate, it keeps a thread alive forever,
 /// so don't run too many test (preferable just one) in sequence.
-pub fn terminates<F>(duration_ms: u64, f: F) -> bool 
+pub fn terminates<F>(duration_ms: u64, f: F) -> bool
 where F: Send + FnOnce() -> () + 'static,
 {
     terminates_async(duration_ms, f, || {})
 }
 
-/// Check if a function `f` terminates within a given timeframe, 
+/// Check if a function `f` terminates within a given timeframe,
 /// but run a second function `g` concurrently.
 ///
 /// If the function does not terminate, it keeps a thread alive forever,
 /// so don't run too many test (preferable just one) in sequence.
-pub fn terminates_async<F, G>(duration_ms: u64, f: F, g: G) -> bool 
+pub fn terminates_async<F, G>(duration_ms: u64, f: F, g: G) -> bool
 where F: Send + FnOnce() -> () + 'static,
       G: FnOnce() -> ()
 {
-    async(duration_ms, f, g).is_some()
+    run_async(duration_ms, f, g).is_some()
 }
 
 /// Run two functions `f` and `g` concurrently.
@@ -37,7 +37,7 @@ where F: Send + FnOnce() -> () + 'static,
 ///
 /// If `f` does not terminate, it keeps a thread alive forever,
 /// so don't run too many test (preferable just one) in sequence.
-pub fn async<T, F, G>(duration_ms: u64, f: F, g: G) -> Option<T>
+pub fn run_async<T, F, G>(duration_ms: u64, f: F, g: G) -> Option<T>
 where F: Send + FnOnce() -> T + 'static,
       G: FnOnce() -> (),
       T: Send + 'static,
@@ -49,9 +49,9 @@ where F: Send + FnOnce() -> T + 'static,
         // wakeup other thread
         let _ = tx.send(t);
     });
-    
+
     g();
-    
+
     if let a@Some(_) = rx.try_recv().ok() {
         return a;
     }
